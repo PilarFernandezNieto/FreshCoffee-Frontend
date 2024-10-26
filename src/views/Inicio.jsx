cd0d33633d2bfc992c4e6d66066a6be24af6fc96
Pilar Fernández Nieto <pilarfnieto@gmail.com>
2024-10-26 13:41:39 +0200

    SWR

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,20 +1,53 @@
-import { productos as data } from "../data/productos"
-import Producto from "../components/Producto"
-import useQuiosco from "../hooks/useQuiosco"
+import useSWR from "swr";
+import Producto from "../components/Producto";
+import useQuiosco from "../hooks/useQuiosco";
+import clienteAxios from "../config/axios";
 
 export default function Inicio() {
-const { categoriaActual } = useQuiosco()
-const productos = data.filter(producto => producto.categoria_id === categoriaActual.id)
+  const { categoriaActual } = useQuiosco();
+
+
+  // Consulta SWR
+  const fetcher = () =>
+    clienteAxios("/api/productos").then((data) => data.data);
+  
+  const { data, error, isLoading } = useSWR("/api/productos", fetcher, {
+    refreshInterval: 1000
+  });
+
+  if (isLoading)
+    return (
+      <div className="sk-circle">
+        <div className="sk-circle1 sk-child"></div>
+        <div className="sk-circle2 sk-child"></div>
+        <div className="sk-circle3 sk-child"></div>
+        <div className="sk-circle4 sk-child"></div>
+        <div className="sk-circle5 sk-child"></div>
+        <div className="sk-circle6 sk-child"></div>
+        <div className="sk-circle7 sk-child"></div>
+        <div className="sk-circle8 sk-child"></div>
+        <div className="sk-circle9 sk-child"></div>
+        <div className="sk-circle10 sk-child"></div>
+        <div className="sk-circle11 sk-child"></div>
+        <div className="sk-circle12 sk-child"></div>
+      </div>
+    );
+
+  const productos = data.data.filter(
+    (producto) => producto.categoria_id === categoriaActual.id
+  );
 
   return (
-   <>
-   <h1 className="text-4xl font-black">{categoriaActual.nombre}</h1>
-   <p className="text-2xl my-10">Elige y personaliza tu pedido a continuación</p>
-   <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-    {productos.map(producto => (
-      <Producto key={producto.imagen} producto={producto} />
-    ))}
-   </div>
-   </>
-  )
+    <>
+      <h1 className="text-4xl font-black">{categoriaActual.nombre}</h1>
+      <p className="text-2xl my-10">
+        Elige y personaliza tu pedido a continuación
+      </p>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+        {productos.map((producto) => (
+          <Producto key={producto.imagen} producto={producto} />
+        ))}
+      </div>
+    </>
+  );
 }
