7d92084ea56826662573539848424ca5b7adcc22
Pilar Fernández Nieto <pilarfnieto@gmail.com>
2024-10-27 12:25:57 +0100

    Muestra errores de validación

---
+++ src/components/Alerta.jsx
@@ -0,0 +1,5 @@
+export default function Alerta({children}) {
+  return (
+    <div className="text-center my-2 bg-red-600 text-white font-bold p-2 uppercase">{children}</div>
+  )
+}
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export default clienteAxios;
