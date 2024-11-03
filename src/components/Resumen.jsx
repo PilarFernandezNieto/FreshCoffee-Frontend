a624dfa6f1192619ea030ba9b862a1aadc5c3936
Pilar Fernández Nieto <pilarfnieto@gmail.com>
2024-11-03 09:35:38 +0100

    graba pedido

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -1,11 +1,19 @@
 import { formatearDinero } from "../helpers";
 import useQuiosco from "../hooks/useQuiosco";
+import {useAuth} from "../hooks/useAuth";
 import ResumenProducto from "./ResumenProducto";
 
 export default function Resumen() {
-  const { pedido, total } = useQuiosco();
+  const { pedido, total, handleSubmitNuevaOrden } = useQuiosco();
+  const {logout} = useAuth({});
   const comprobarPedido = () => pedido.length === 0;
 
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    handleSubmitNuevaOrden(logout);
+
+  }
   return (
     <aside className="md:w-72 h-screen overflow-y-scroll p-5">
       <h1 className="text-4xl font-black">Mi Pedido</h1>
@@ -24,13 +32,20 @@ export default function Resumen() {
       </div>
 
       <p className="text-xl mt-10">Total: {formatearDinero(total)}</p>
-      <form action="" className="w-full">
+      <form 
+        className="w-full"
+        onSubmit={handleSubmit}
+      >
         <div className="mt-5">
           <input
             type="submit"
             value="Confirmar pedido"
             disabled={comprobarPedido()}
-            className={`${comprobarPedido() ? 'bg-indigo-200' : 'bg-indigo-600 hover:bg-indigo-800'} px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer`}
+            className={`${
+              comprobarPedido()
+                ? "bg-indigo-200"
+                : "bg-indigo-600 hover:bg-indigo-800"
+            } px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer`}
           />
         </div>
       </form>
--- src/context/QuioscoProvider.jsx
+++ src/context/QuioscoProvider.jsx
@@ -1,92 +1,135 @@
 import { createContext, useState, useEffect } from "react";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 import clienteAxios from "../config/axios";
 const QuioscoContext = createContext();
 
 const QuioscoProvider = ({ children }) => {
+  const [categorias, setCategorias] = useState([]);
+  const [categoriaActual, setCategoriaActual] = useState({});
+  const [modal, setModal] = useState(false);
+  const [producto, setProducto] = useState({});
+  const [pedido, setPedido] = useState([]);
+  const [total, setTotal] = useState(0);
 
-    const [categorias, setCategorias] = useState([]);
-    const [categoriaActual, setCategoriaActual] = useState({})
-    const [modal, setModal] = useState(false)
-    const [producto, setProducto] = useState({})
-    const [pedido, setPedido] = useState([])
-    const [total, setTotal] = useState(0)
+  useEffect(() => {
+    const nuevoTotal = pedido.reduce(
+      (total, producto) => producto.precio * producto.cantidad + total,
+      0
+    );
+    setTotal(nuevoTotal);
+  }, [pedido]);
 
-    useEffect(() => {
-      const nuevoTotal = pedido.reduce((total, producto) => (producto.precio * producto.cantidad) + total, 0)
-      setTotal(nuevoTotal)
+  const obtenerCategorias = async () => {
+    try {
+      const { data } = await clienteAxios("/api/categorias");
+      setCategorias(data.data);
+      setCategoriaActual(data.data[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    obtenerCategorias();
+  }, []);
+
+  const handleClickCategoria = (id) => {
+    const categoria = categorias.filter((categoria) => categoria.id === id)[0];
+    setCategoriaActual(categoria);
+  };
+
+  const handleClickModal = () => {
+    setModal(!modal);
+  };
+  const handleSetProducto = (producto) => {
+    setProducto(producto);
+  };
+
+  const handleAgregarPedido = ({ categoria_id, imagen, ...producto }) => {
+    // ...pedido -> crea una copia del array original ya que no se puede modificar,
+    // y añade un elemento producto a ese pedido
+
+    if (pedido.some((pedidoState) => pedidoState.id === producto.id)) {
+      const pedidoActualizado = pedido.map((pedidoState) =>
+        pedidoState.id === producto.id ? producto : pedidoState
+      );
+      setPedido(pedidoActualizado);
+      toast.success("Guardado correctamente");
+    } else {
+      setPedido([...pedido, producto]);
+      toast.success("Se ha añadido al pedido");
+    }
+  };
+
+  const handleEditarCantidad = (id) => {
+    const productoActualizar = pedido.filter(
+      (producto) => producto.id === id
+    )[0];
+    setProducto(productoActualizar);
+    setModal(!modal);
+  };
+
+  const handleEliminarProductoPedido = (id) => {
+    const pedidoActualizado = pedido.filter((producto) => producto.id !== id);
+    setPedido(pedidoActualizado);
+    toast.success("Producto eliminado");
+  };
+
+  const handleSubmitNuevaOrden = async (logout) => {
+    const token = localStorage.getItem("AUTH_TOKEN");
+
+    try {
+      const { data } = await clienteAxios.post(
+        "/api/pedidos",
+        {
+          total,
+          productos: pedido.map((producto) => {
+            return {
+              id: producto.id,
+              cantidad: producto.cantidad,
+            };
+          }),
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      toast.success(data.message);
+
+      setTimeout(() => {
+        setPedido([]);
+      }, 1000);
+
+      // Cerrar la sesión del usuario
+      setTimeout(() => {
+        localStorage.removeItem("AUTH_TOKEN");
+        logout();
+      }, 3000);
       
-    }, [pedido])
-
-    const obtenerCategorias = async () => {
-
-      try {
-        const {data} = await clienteAxios("/api/categorias");
-        setCategorias(data.data);
-        setCategoriaActual(data.data[0]);
-        
-      } catch (error) {
-        console.log(error);
-      }
+    } catch (error) {
+      console.log(error);
     }
-    useEffect(() => {
-      obtenerCategorias();
-    }, [])
-
-    const handleClickCategoria = id => {
-        const categoria = categorias.filter(categoria => categoria.id === id)[0]
-        setCategoriaActual(categoria)
-    }
-
-    const handleClickModal = () => {
-        setModal(!modal)
-    }
-    const handleSetProducto = producto => {
-        setProducto(producto)
-    }
-
-    const handleAgregarPedido = ({categoria_id, imagen, ...producto}) => {
-      // ...pedido -> crea una copia del array original ya que no se puede modificar, 
-      // y añade un elemento producto a ese pedido
-    
-      if(pedido.some( pedidoState => pedidoState.id === producto.id)){
-        const pedidoActualizado = pedido.map(pedidoState => pedidoState.id === producto.id ? producto : pedidoState )
-        setPedido(pedidoActualizado)
-        toast.success("Guardado correctamente")
-      } else {
-        setPedido([...pedido, producto])
-        toast.success("Se ha añadido al pedido")
-      }
-    }
-
-    const handleEditarCantidad = id => {
-      const productoActualizar = pedido.filter(producto => producto.id === id)[0]
-      setProducto(productoActualizar)
-      setModal(!modal)
-    }
-
-    const handleEliminarProductoPedido = id => {
-      const pedidoActualizado = pedido.filter(producto => producto.id !== id)
-      setPedido(pedidoActualizado)
-      toast.success("Producto eliminado")
-    }
-
+  };
 
   return (
     <QuioscoContext.Provider
       value={{
-       categorias,
-       categoriaActual,
-       handleClickCategoria,
-       modal,
-       handleClickModal,
-       producto,
-       handleSetProducto,
-       pedido,
-       handleAgregarPedido,
-       handleEditarCantidad,
-       handleEliminarProductoPedido,
-       total
+        categorias,
+        categoriaActual,
+        handleClickCategoria,
+        modal,
+        handleClickModal,
+        producto,
+        handleSetProducto,
+        pedido,
+        handleAgregarPedido,
+        handleEditarCantidad,
+        handleEliminarProductoPedido,
+        total,
+        handleSubmitNuevaOrden,
       }}
     >
       {children}
